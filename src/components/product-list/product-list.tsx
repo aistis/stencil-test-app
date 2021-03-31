6a5e40ff3795523fe074b60d4fc726df23c6b99a
Aistis <aistis.gasparavicius@scandiweb.com>
2021-04-01 01:14:40 +0300

    Template and styling for product list component

--- src/components/product-list/product-list.tsx
+++ src/components/product-list/product-list.tsx
@@ -1,4 +1,6 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, Prop, State, h } from '@stencil/core';
+import { endpoints } from "../../helpers/apiEndpointsConfig";
+import state from '../../helpers/store';
 
 @Component({
   tag: 'product-list',
@@ -6,12 +8,45 @@ import { Component, Host, h } from '@stencil/core';
   shadow: true,
 })
 export class ProductList {
+  @Prop() brand: string;
+  @State() data= [];
+  @State() sortState = false;
+
+  async componentWillLoad() {
+    try {
+      const response = await fetch(endpoints.products)
+      const data = await response.json()
+      // TODO:needs to be handled in case of fetch or parse fail
+      state.products = data
+      this.data = data.filter(product => product.brand.toLowerCase() == this.brand)
+    } catch (error) {
+      console.error(error)
+    }
+    console.log(this.data)
+  }
 
   render() {
     return (
-      <Host>
-        <slot></slot>
-      </Host>
+      <ion-content class="ion-padding">
+        <page-headline text={`Here is the ${this.brand} phones list`} />
+        <ion-grid>
+          <ion-row>
+            <ion-col sizeMd="8">
+              <ion-item>
+                <ion-label>Sort items by title ascending</ion-label>
+                <ion-toggle checked={this.sortState} onIonChange={(ev) => (this.sortState = ev.detail.checked)} />
+              </ion-item>
+            </ion-col>
+          </ion-row>
+          <ion-row>
+            {this.data.length == 0
+                ? <h3>No data loaded yet</h3>
+                : this.data.map((item) =>
+                <product-card item={item} productType={true}></product-card>
+              )}
+          </ion-row>
+        </ion-grid>
+      </ion-content>
     );
   }
 
