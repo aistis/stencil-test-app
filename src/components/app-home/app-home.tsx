e0449fc8121e6ecd1a4b72d1f4009c13c765f410
Aistis <aistis.gasparavicius@scandiweb.com>
2021-03-31 22:07:49 +0300

    Home component template and style adjustments

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -1,27 +1,33 @@
-import { Component, h } from '@stencil/core';
+import { Component, State, h } from '@stencil/core';
 
 @Component({
   tag: 'app-home',
   styleUrl: 'app-home.css',
 })
 export class AppHome {
+
   render() {
     return [
-      <ion-header>
-        <ion-toolbar color="primary">
-          <ion-title>Home</ion-title>
-        </ion-toolbar>
-      </ion-header>,
-
       <ion-content class="ion-padding">
-        <p>
-          Welcome to the PWA Toolkit. You can use this starter to build entire apps with web components using Stencil and ionic/core! Check out the README for everything that comes
-          in this starter out of the box and check out our docs on <a href="https://stenciljs.com">stenciljs.com</a> to get started.
-        </p>
-
-        <ion-button href="/profile/ionic" expand="block">
-          Profile page
-        </ion-button>
+        <ion-text color="tertiary">
+          <h1>
+            Mega store featured categories
+          </h1>
+        </ion-text>
+        <ion-card>
+          <ion-img
+            src="https://i.picsum.photos/id/103/900/300.jpg?hmac=2r71Jgs7llvbcembHL8qVOKq5pB77kHZRlZgxAl5RvY"
+            alt="Mobiltelefoner"
+          ></ion-img>
+          <ion-card-header>
+            <ion-card-subtitle>Phones</ion-card-subtitle>
+            <ion-card-title><h2>Mobiltelefoner</h2></ion-card-title>
+          </ion-card-header>
+          <ion-card-content>
+          No phone, no lights, no motor car, not a single luxury. Like Robinson Crusoe it's primitive as can be. He's gaining' on you so you better look alive. He's busy revin' up his Powerful Mach 5. Straightnin' the curves. Flatnin the hills. Someday the mountain might get 'em but the law never will.
+          </ion-card-content>
+          <ion-button color="tertiary" href="/phones">Phones</ion-button>
+        </ion-card>
       </ion-content>,
     ];
   }
