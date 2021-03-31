b2dd63424254838fbee055eb593193e349b68e11
Aistis <aistis.gasparavicius@scandiweb.com>
2021-03-31 21:15:27 +0300

    Defined endpoints

---
+++ src/helpers/apiEndpointsConfig.ts
@@ -0,0 +1,4 @@
+export const endpoints = {
+  landing : 'https://raw.githubusercontent.com/TeliaSweden/frontend-interview-api/master/brands.json',
+  products : 'https://raw.githubusercontent.com/TeliaSweden/frontend-interview-api/master/phones.json'
+}
